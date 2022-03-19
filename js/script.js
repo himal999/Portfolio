d5eb4b09697b4cf59c90bbbd5aeaf177886b8c75
himal999 <himal.official99@gmail.com>
2022-03-19 18:31:26 +0530

    add jquery in control  active deactive btn

--- js/script.js
+++ js/script.js
@@ -4,15 +4,55 @@ const sectBtn = document.querySelectorAll('.control')//div
 const allSections = document.querySelectorAll('.main-content')
 
 
-function PageTransitions(){
-    //button click active class
-    for(let i=0;i<sectBtn.length;i++){
-        sectBtn[i].addEventListener('click',function(){
-            let currentBtn = document.querySelectorAll('.active-btn')
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
-            this.className += 'active-btn'
-        })
+//start of active btn deactive btn
+
+// control btn active/deactive in javascript
+
+// function PageTransitions(){
+//     //button click active class
+//     for(let i=0;i<sectBtn.length;i++){
+//         sectBtn[i].addEventListener('click',function(){
+//             let currentBtn = document.querySelectorAll('.active-btn')
+//             currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
+//             this.className += 'active-btn'
+//         })
+//     }
+// }
+
+// PageTransitions();
+
+
+
+// control btn active/deactive in jQuery
+
+
+const controlBtn  = $('.control');
+
+function pageTt(){
+   
+    for(let i=0;i<controlBtn.length;i++){
+       
+       $('.control').on("click",function(){
+           
+            let curr = $('.active-btn')
+            curr.removeClass('active-btn')
+            $(this).addClass('active-btn')
+      
+        });
     }
 }
 
-PageTransitions();
+
+pageTt();
+
+
+//end of active btn deactive btn
+
+//
+//
+//
+//
+//
+//
+//
+//
